96609c9dee324f2f260cce3af1a98640b036176f
Uros <uros.randelovic@gmail.com>
2020-07-07 01:32:53 -0400

    - change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, HashRouter, Route} from "react-router-dom";
 // we will create the component in a bit - it will show an error for the time being
 import HomePage from "./Landing Pages/HomePage";
 
 function App() {
   return (
-      <Router>
+      <HashRouter basename={'/'}>
         <div className="App">
           <Route exact path={"/"} component={HomePage}/>
         </div>
-      </Router>
+      </HashRouter>
   );
 }
 
