1b8cceeb00bf612a6655f2cf6bc63d36f15ad9cf
Uros <uros.randelovic@gmail.com>
2020-07-07 00:36:32 -0400

    Updates

---
+++ precache-manifest.9f9b52a0caf8f26d312fb264f882c430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1930381a7be76e267a0fd6c853f7885e",
+    "url": "/posterboi/index.html"
+  },
+  {
+    "revision": "a59f5907898b8a33d629",
+    "url": "/posterboi/static/js/2.c4d40f2a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/posterboi/static/js/2.c4d40f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa8af04920a5f19084c",
+    "url": "/posterboi/static/js/main.31f0cdf5.chunk.js"
+  },
+  {
+    "revision": "bcfbea3eda4434b46ffb",
+    "url": "/posterboi/static/js/runtime-main.47247946.js"
+  },
+  {
+    "revision": "6c6c6a5aacd7306f035bd8e70c3ccbcd",
+    "url": "/posterboi/static/media/hompageCover.6c6c6a5a.jpg"
+  }
+]);
